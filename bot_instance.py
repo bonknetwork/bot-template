ae61b0532d0bcffb360f09b62f8daec69add2fed
Pyro <67813460+shloakvatsyayan@users.noreply.github.com>
2025-01-17 19:26:42 -0500

    Turned into bot template

--- bot_instance.py
+++ bot_instance.py
@@ -2,14 +2,15 @@ from utils import config
 from interactions import Client, check, SlashContext
 import os
 
-# Get Bonk Network bot token and create bot client
+
 AppConfig_obj = config.AppConfig()
 token = AppConfig_obj.get_bonk_staff_key()
 bot = Client(token=token, sync_interactions=True)
 
-CHECK_ROLES = [1282491372250857676, 1263837661605527603, 1259828028012232714]
 
-# Role check decorator for role-specific commands
+
+# Example role check function, make sure to change the role IDs.
+CHECK_ROLES = [1111111111111111111, 2222222222222222222]
 def role_check():
     async def predicate(ctx: SlashContext):
         user_roles = [int(i.id) for i in ctx.author.roles]
